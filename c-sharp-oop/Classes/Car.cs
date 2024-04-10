37df35f469a152942d43295c10e35a6df2f39aad
ralfxdev <lopezalexander1019@gmail.com>
2024-04-09 21:55:42 -0600

    Add project files.

---
+++ c-sharp-oop/Classes/Car.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_oop.Classes
+{
+    internal class Car
+    {
+        public string Brand { get; }
+        public int Model { get; }
+        public int MaxSpeed { get; }
+        public string? Color { get; set; }
+        public string? Owner { get; set; }
+        public string? Price { get; set; }
+        public string? Plate { get; set; }
+
+        public bool IsOn = false;
+        public bool IsLookDoors = false;
+        public bool IsLightsOn = false;
+
+        // 0 = Neutral
+        private int _Gear = 0;
+        private bool _Clutch = false;
+        private int _SpeedCurrent = 0;
+        private double _FuelLevel = 0;
+
+        public Car(string brand, int model, int maxSpeed)
+        {
+            Brand = brand;
+            Model = model;
+            MaxSpeed = maxSpeed;
+        }
+
+        public void SwitchCar()
+        {
+            IsOn = !IsOn;
+        }
+
+        public void Clutch()
+        {
+            _Clutch = !_Clutch;
+        }
+
+        public int Accelerate()
+        {
+            if (IsOn)
+            {
+                if (_SpeedCurrent < MaxSpeed && _Gear > 0 && _FuelLevel > 0)
+                {
+                    _SpeedCurrent += 10;
+                    _FuelLevel -= _SpeedCurrent * 0.1;
+                }   
+            }
+            return _SpeedCurrent;
+        }
+
+        public int Decelerate()
+        {
+            if (IsOn)
+            {
+                if (_SpeedCurrent > 0 && _Gear > 0)
+                {
+                    _SpeedCurrent -= 10;
+                }
+            }
+
+            return _SpeedCurrent;
+        }
+
+        public int Brake()
+        {
+           return _SpeedCurrent = 0;
+        }
+
+        public int ShiftUp()
+        {
+            if (_Gear < 5 && _Clutch)
+            {
+                _Gear++;
+            }
+            return _Gear;
+        }
+
+        public int ShiftDown()
+        {
+            if (_Gear > 0 && _Clutch)
+            {
+                _Gear--;
+            }
+            return _Gear;
+        }
+
+        public void SwitchDoors()
+        {
+            IsLookDoors = !IsLookDoors;
+        }
+
+        public void SwitchLights()
+        {
+            IsLightsOn = !IsLightsOn;
+        }
+
+        public void Refuel(double liters)
+        {
+            _FuelLevel += liters;
+        }
+
+        public double GetFuelLevel()
+        {
+            return _FuelLevel;
+        }
+
+    }
+}
---
+++ c-sharp-oop/Program.cs
@@ -0,0 +1,125 @@
+﻿using c_sharp_oop.Classes;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+
+Car honda = new Car("Honda", 2019, 120);
+
+honda.Color = "Negro";
+honda.Owner = "ralfxdev";
+honda.Price = "$1,000,000.00";
+honda.Plate = "A123456";
+
+Console.WriteLine("Datos del Carro\n");
+Console.WriteLine($"Marca: {honda.Brand}");
+Console.WriteLine($"Modelo: {honda.Model}");
+Console.WriteLine($"Velocidad Máxima: {honda.MaxSpeed}km/h");
+Console.WriteLine($"Color: {honda.Color}");
+Console.WriteLine($"Dueño: {honda.Owner}");
+Console.WriteLine($"Precio: {honda.Price}");
+Console.WriteLine($"Placa: {honda.Plate}\n");
+
+honda.SwitchCar();
+
+if (honda.IsOn)
+{
+    Console.WriteLine("Simulador de Viaje\n");
+
+    if (honda.IsLookDoors)
+    {
+        Console.WriteLine("Las puertas están abiertas\n");
+    } else
+    {
+        Console.WriteLine("Las puertas están cerradas\n");
+    }
+
+    honda.SwitchLights();
+    if (honda.IsLightsOn)
+    {
+        Console.WriteLine("Las luces están encendidas\n");
+    } else
+    {
+        Console.WriteLine("Las luces están apagadas\n");
+    }
+
+    honda.Refuel(100);
+
+    if (honda.GetFuelLevel() > 0)
+    {
+        Console.WriteLine($"El tanque tiene -> {honda.GetFuelLevel()}lts\n");
+    } else
+    {
+        Console.WriteLine("El tanque no está lleno\n");
+    }
+
+    Console.WriteLine("El carro está encendido\n");
+    honda.Clutch();
+    Console.WriteLine($"Subiste un cambio -> {honda.ShiftUp()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Subiste un cambio -> {honda.ShiftUp()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h\n");
+    Console.WriteLine($"Frenaste -> {honda.Brake()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Bajaste un cambio -> {honda.ShiftDown()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Subiste un cambio -> {honda.ShiftUp()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Subiste un cambio -> {honda.ShiftUp()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Subiste un cambio -> {honda.ShiftUp()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Subiste un cambio -> {honda.ShiftUp()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h");
+    Console.WriteLine($"Aceleraste -> {honda.Accelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Bajaste un cambio -> {honda.ShiftDown()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h");
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Bajaste un cambio -> {honda.ShiftDown()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h");
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Bajaste un cambio -> {honda.ShiftDown()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h");
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Bajaste un cambio -> {honda.ShiftDown()}\n");
+    honda.Clutch();
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h");
+    Console.WriteLine($"Desaceleraste -> {honda.Decelerate()} km/h\n");
+    Console.WriteLine($"Frenaste -> {honda.Brake()} km/h\n");
+    honda.Clutch();
+    Console.WriteLine($"Bajaste un cambio -> {honda.ShiftDown()}\n");
+    honda.Clutch();
+
+    honda.SwitchCar();
+    Console.WriteLine($"Tu tanque tiene -> {honda.GetFuelLevel()}lts");
+} else
+{
+    Console.WriteLine("El carro está apagado");
+}
